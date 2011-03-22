0019d15ee73839ef7c78babd3ba54da6bd7ed81b
Wade Simmons <wade@wades.im>
2011-03-21 23:22:18 -0600

    use rm -rf if trash isn't installed

--- mailplaner.sh
+++ mailplaner.sh
@@ -2,7 +2,11 @@
 
 SUFFIX=SimpleGeo
 
-trash /Applications/Mailplane${SUFFIX}.app
+if which trash >/dev/null 2>&1; then
+    trash /Applications/Mailplane${SUFFIX}.app
+else
+    rm -rf /Applications/Mailplane${SUFFIX}.app
+fi
 cp -a /Applications/Mailplane.app /Applications/Mailplane${SUFFIX}.app
 if [ -e Mailplane.icns ]; then
     cp Mailplane.icns /Applications/Mailplane${SUFFIX}.app/Contents/Resources
